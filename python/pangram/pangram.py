2b07ae4e6fdebdb4723265bb6e5eff170f859ced
John Dewey <john@dewey.ws>
2016-02-21 14:26:31 -0800

    pangram submission

---
+++ python/pangram/pangram.py
@@ -0,0 +1,15 @@
+import string
+
+
+def is_pangram(phrase):
+    """ Pangrams are words or sentences containing every letter
+    of the alphabet at least once; the best known English example
+    being A quick brown fox jumps over the lazy dog.
+    """
+    seen = dict.fromkeys(string.ascii_lowercase, 0)
+    for c in list(phrase):
+        seen[c.lower()] = 1
+
+    if 0 in seen.values():
+        return False
+    return True

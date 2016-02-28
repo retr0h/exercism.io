27b3c89cc13eebd71c9cf7cbc701adf9ff474855
John Dewey <john@dewey.ws>
2016-02-28 12:36:03 -0800

    Didn't need the setters/getters

--- python/robot-simulator/robot_simulator.py
+++ python/robot-simulator/robot_simulator.py
@@ -6,37 +6,13 @@ WEST = 3
 
 class Robot(object):
     def __init__(self, bearing=NORTH, x=0, y=0):
-        self._x = x
-        self._y = y
-        self._bearing = bearing
-
-    @property
-    def x(self):
-        return self._x
-
-    @x.setter
-    def x(self, value):
-        self._x = value
-
-    @property
-    def y(self):
-        return self._y
-
-    @y.setter
-    def y(self, value):
-        self._y = value
+        self.x = x
+        self.y = y
+        self.bearing = bearing
 
     @property
     def coordinates(self):
         return self.x, self.y
-
-    @property
-    def bearing(self):
-        return self._bearing
-
-    @bearing.setter
-    def bearing(self, value):
-        self._bearing = value
 
     def turn_right(self):
         self.bearing = (self.bearing + 1) % 4
@@ -55,7 +31,6 @@ class Robot(object):
             self.x += -1
 
     def simulate(self, instructions):
-        print list(instructions)
         for instruction in list(instructions):
             if instruction == 'L':
                 self.turn_left()
